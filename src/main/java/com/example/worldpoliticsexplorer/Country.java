0e0d8c84d89c1a96d01991cf3a72359630c6e3dd
Kerem1989 <98690898+Kerem1989@users.noreply.github.com>
2024-07-05 22:06:21 +0200

    Initial commit

---
+++ src/main/java/com/example/worldpoliticsexplorer/Country.java
@@ -0,0 +1,80 @@
+package com.example.worldpoliticsexplorer;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Country {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String capital;
+    private int population;
+
+    private String rulingParty;
+
+    private String ideology;
+
+    public Country() {
+    }
+
+    public Country(String name, String capital, int population, String rulingParty, String ideology) {
+        this.name = name;
+        this.capital = capital;
+        this.population = population;
+        this.rulingParty = rulingParty;
+        this.ideology = ideology;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public void setCapital(String capital) {
+        this.capital = capital;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    public String getRulingParty() {
+        return rulingParty;
+    }
+
+    public void setRulingParty(String rulingParty) {
+        this.rulingParty = rulingParty;
+    }
+
+    public String getIdeology() {
+        return ideology;
+    }
+
+    public void setIdeology(String ideology) {
+        this.ideology = ideology;
+    }
+}
---
+++ src/main/java/com/example/worldpoliticsexplorer/WorldPoliticsExplorerApplication.java
@@ -0,0 +1,13 @@
+package com.example.worldpoliticsexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldPoliticsExplorerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorldPoliticsExplorerApplication.class, args);
+    }
+
+}
